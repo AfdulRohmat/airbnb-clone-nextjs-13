946a0503c0ed5e097b08a997a6e1de2657897e84
Afdul Rohmat <afdulrohmat03@gmail.com>
2023-05-03 01:55:59 +0700

    create categories ui using next navigation and queryString package

---
+++ app/components/CategoryBox.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import { IconType } from "react-icons";
+import queryString from "query-string";
+
+interface CategoryBoxProps {
+  icon: IconType;
+  label: string;
+  selected?: boolean;
+}
+
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = queryString.parse(params.toString());
+    }
+
+    // used to parse something as url param
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    // reset exisiting category so another category can replace it when user click another category
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = queryString.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+
+  return (
+    <div
+      onClick={() => handleClick()}
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-rose-400 transition cursor-pointer
+  ${selected ? "border-b-rose-500" : "border-transparent"}
+  ${selected ? "text-rose-500" : "text-neutral-500"}
+`}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">{label}</div>
+    </div>
+  );
+};
+
+export default CategoryBox;
